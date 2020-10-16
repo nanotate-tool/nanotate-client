eb97aa60482560fbae2eda8cc61aedce837ac7e7
Jose Miguel Ruano <miguel199308@gmail.com>
2020-10-15 20:12:15 -0500

    module nanopub-card component added

--- src/app/components/nanopub-publisher/nanopub-publisher.module.ts
+++ src/app/components/nanopub-publisher/nanopub-publisher.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NanopubPublisherComponent } from './nanopub-publisher.component';
 import { MenubarModule } from "primeng/menubar";
 import { NanopubRdfBodyModule } from '../nanopub-rdf-body/nanopub-rdf-body.module';
+import { NanopubCardModule } from '../nanopub-card/nanopub-card.module';
 
 @NgModule({
   declarations: [NanopubPublisherComponent],
   imports: [
     CommonModule,
     MenubarModule,
-    NanopubRdfBodyModule
+    NanopubRdfBodyModule,
+    NanopubCardModule
   ],
   exports: [NanopubPublisherComponent]
 })
