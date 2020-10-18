aee48710b5cac09b87cbf729499badaea31652b0
Jose Miguel Ruano <miguel199308@gmail.com>
2020-10-18 13:57:51 -0500

    nanopubs-central-page updated
    updated loading effects

--- src/app/nanopubs/nanopubs.component.ts
+++ src/app/nanopubs/nanopubs.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '../services';
+import { AppService, HypothesisService } from '../services';
 import { MenuItem } from 'primeng/api';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BaseSubscriptionComponent } from '../utils';
 
 @Component({
   templateUrl: './nanopubs.component.html'
 })
-export class NanopubsComponent implements OnInit {
+export class NanopubsComponent extends BaseSubscriptionComponent implements OnInit {
 
+  loaded: boolean = false;
   tabs: MenuItem[];
   activeTab: MenuItem;
 
-  constructor(private app: AppService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private app: AppService, private router: Router, private activatedRoute: ActivatedRoute,
+    private hypothesisService: HypothesisService) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.tabs = [
-      { label: 'Nanopublications', icon: 'pi pi-list', routerLink:['.'] },
-      { label: 'Annotations', icon: 'pi i-hypothesis-logo', routerLink: 'register' },
-      { label: 'Stats', icon: 'pi pi-chart-line', routerLink: 'stats' }
-    ];
-    this.activeTab = this.tabs[0];
+    this.loadTabs();
+    this.addSubscription(
+      this.hypothesisService.onProfileChange(profile => {
+        this.loaded = profile && true;
+      })
+    );
   }
 
   goToPage(page: any, query = undefined) {
     this.router.navigate(page, { relativeTo: this.activatedRoute, queryParams: query })
+  }
+
+  loadTabs() {
+    this.tabs = [
+      { label: 'Nanopublications', icon: 'pi pi-list', routerLink: ['.'] },
+      { label: 'Annotations', icon: 'pi i-hypothesis-logo', routerLink: 'register' },
+      { label: 'Stats', icon: 'pi pi-chart-line', routerLink: 'stats' }
+    ];
+    this.activeTab = this.tabs[0];
   }
 
 }
